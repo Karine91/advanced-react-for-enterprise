2233c95d95d9251cf7cab7b97575ac9dad9f9fef
karine <gulkarine@gmail.com>
2023-03-16 19:24:50 +0200

    chore: finish css section, add build script for all components

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -1,7 +1,31 @@
-const fs = require('fs');
-const path = require('path');
-const sass = require('sass');
+const fs = require("fs");
+const path = require("path");
+const sass = require("sass");
 
-const result = sass.compile(path.resolve('src/global.scss'), { sync: true });
+const getComponents = () => {
+  let allComponents = [];
 
-fs.writeFileSync(path.resolve('src/lib/global.css'), result.css.toString())
+  const types = ["atoms", "molecules", "organisms"];
+
+  types.forEach((type) => {
+    const allFiles = fs.readdirSync(`src/${type}`).map((file) => ({
+      input: path.resolve("src", type, file),
+      output: path.resolve("src", "lib", file.slice(0, -4) + "css"),
+    }));
+
+    allComponents = [...allComponents, ...allFiles];
+  });
+
+  return allComponents;
+};
+
+const compile = (filePath, fileName) => {
+  const result = sass.compile(path.resolve(filePath), { sync: true });
+  fs.writeFileSync(path.resolve(fileName), result.css.toString());
+};
+
+compile("src/global.scss", "src/lib/global.css");
+
+getComponents().forEach((component) =>
+  compile(component.input, component.output)
+);
